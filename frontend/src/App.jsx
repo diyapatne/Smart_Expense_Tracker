d952fc645a69fe3235ff53ed721d5a663a1087f5
Manasi Talele <manasi@Manasis-MacBook-Air.local>
2026-06-17 21:45:20 +0530

    .env and app.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Smart Receipt & Expense Tracker</h1>
+
+      <h2>Hello World</h2>
+
+      <p>Day 1 Coding Complete</p>
+    </div>
+  );
+}
+
+export default App;
